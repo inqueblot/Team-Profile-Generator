d1b215e8312e5f0dbdcfccb24f1e30c54065d8ea
inqueblot <notminebydesign@gmail.com>
2021-01-31 02:38:08 -0600

    engineer.js pass test, set 2 extend employee class

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -1,19 +1,19 @@
 const Engineer = require("../lib/Engineer");
 
 test("Can set GitHUb account via constructor", () => {
-  const testValue = "GitHubUser";
+  const testValue = "GithubUser";
   const e = new Engineer("Foo", 1, "test@test.com", testValue);
-  expect(e.github).toBe(testValue);
+  expect(e.githubUser).toBe(testValue);
 });
 
 test("getRole() should return \"Engineer\"", () => {
   const testValue = "Engineer";
-  const e = new Engineer("Foo", 1, "test@test.com", "GitHubUser");
+  const e = new Engineer("foo", 1, "testa@test.com", "GitHubUser");
   expect(e.getRole()).toBe(testValue);
 });
 
-test("Can get GitHub username via getGithub()", () => {
-  const testValue = "GitHubUser";
+test("Can get GitHub username via getGithubUser()", () => {
+  const testValue = "githubUser";
   const e = new Engineer("Foo", 1, "test@test.com", testValue);
-  expect(e.getGithub()).toBe(testValue);
+  expect(e.getGithubUser()).toBe(testValue);
 });
