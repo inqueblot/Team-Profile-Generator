b5d17a50968a74be21cbe4cb9adbcc45237e396d
inqueblot <notminebydesign@gmail.com>
2021-01-31 19:32:11 -0600

    able to add intern to team, added intern questions

--- index.js
+++ index.js
@@ -4,50 +4,26 @@ const Employee = require('./lib/Employee')
 const Intern = require('./lib/Intern')
 const Engineer = require('./lib/Engineer')
 const Manager = require('./lib/Manager')
+const { managerQuestions, internQuestions } = require('./lib/Questions')
 
-let managerQuestions = [
-    {
-        name: 'name',
-        message: 'Please enter your name.'
-    },
-    {
-        name: 'id',
-        message: 'Please enter your Employee ID'
-    },
-    {
-        name: 'email',
-        message: 'Please enter your email address.'
-    },
-    {
-        name: 'officeNumber',
-        message: 'Please enter your office number.'
-    },
-    {
-        type: 'expand',
-        name: 'moreTeam',
-        message: `Would you like to add an [i]ntern, [e]ngineer or [f]inish building your team?`,
-        choices: [
-            {
-                key: 'e',
-                name: 'engineer',
-                value: 'engineer'
-            },
-            {
-                key: 'i',
-                name: 'intern',
-                value: 'intern'
-            },
-            {
-                key: 'f',
-                name: 'no more roles to add',
-                value: 'finish'
-            }
-        ]
-    }
-]
+// console.log(Manager)
+// console.log(managerQuestions)
+// console.log(internQuestions)
 
-inquirer.prompt(managerQuestions).then(({ name, id, email, officeNumber }) => {
+
+
+inquirer.prompt(managerQuestions).then(({ name, id, email, officeNumber, moreTeam }) => {
     const manager = new Manager(name, id, email, officeNumber)
+    if (moreTeam === 'intern') {
+        inquirer.prompt(internQuestions).then(({ name, id, email, school, moreTeam }) => {
+            const intern = new Intern(name, id, email, school);
+            console.log(intern);
+        })
+    };
+    if (moreTeam === 'engineer') {
+        engineerQuestion()
+    }
     console.log(manager)
     console.log(manager.getRole())
+    console.log(moreTeam)
 })
